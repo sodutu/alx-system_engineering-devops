0ac8978342e0751835afbfa7bf76a716ff967eec
VictoriaOdutu20 <110996257+VictoriaOdutu20@users.noreply.github.com>
2023-03-02 18:43:06 +0300

    del

--- 0x06-regular_expressions/3-to_json_string.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""return a JSON representation of an object"""
-import json
-
-
-def to_json_string(my_obj):
-    """return JSON"""
-
-    return json.dumps(my_obj)
--- 0x06-regular_expressions/8-class_to_json.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""return the dictionary"""
-
-
-def class_to_json(obj):
-    """Returns the dict descr for JSON"""
-
-    return obj.__dict__
